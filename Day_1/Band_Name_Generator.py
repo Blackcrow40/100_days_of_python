dd6dfa8e21454a4e9b16d6c918f8d2e42272288e
Crow <theblackcrowforty@gmail.com>
2024-07-12 12:14:57 +0200

    Create Band_Name_Generator.py

---
+++ Day_1/Band_Name_Generator.py
@@ -0,0 +1,10 @@
+def main():
+    print(" - - - Welcome To The Band Name Generator! - - - - ")
+    print()
+    city = input("Whats The Name Of The City You Grew Up In?  ")
+    print()
+    pet = input("What Is Yours Pets Name?  ")
+    print()
+    print(f"Your Band Name Could Be {city} {pet}")
+if __name__ == '__main__':
+    main()

62391edbfe6cc9caa19a898f54e0821d48dd8568
Crow <theblackcrowforty@gmail.com>
2024-07-21 19:57:27 +0200

    Update rock_paper_scissors.py

--- Day_4/rock_paper_scissors.py
+++ Day_4/rock_paper_scissors.py
@@ -1,1 +1,67 @@
+#TODO
+# Add a point tracker
+
+import random
+hand = ["Rock", "Paper", "Scissors"]
+
+def main():
+    while True:
+        player_hand = int(input("What do you choose?\n"
+                                "1: for Rock\n"
+                                "2: for Paper\n" 
+                                "3: for Scissors\n"
+                                "--> "))
+        
+        if player_hand == 1:
+            print()
+            print("You Chose Rock!")
+            print(" - - - - - - - - - - - - - - ")
+        
+        elif player_hand == 2:
+            print()
+            print("You Chose Paper!")
+            print(" - - - - - - - - - - - - - - ")
+            
+        elif player_hand == 3:
+            print()
+            print("You Chose Scissors!")
+            print(" - - - - - - - - - - - - - - ")
+        
+        cpu_hand = random.choice(hand)
+        print()
+        print(f"Computer Chose {cpu_hand}")
+        print(" - - - - - - - - - - - - - - ")
+        
+        # Rock (1)
+        if cpu_hand == 'Rock': 
+            if player_hand == 1:
+                print("It Is A Draw")
+            elif player_hand == 2:
+                print("You Win!") 
+            elif player_hand == 3:
+                print("You lose!")       
+            
+            
+        # Paper (2)
+        elif cpu_hand == 'Paper':
+            if player_hand == 1:
+                print("You Lose!")
+            elif player_hand == 2:
+                print("It Is A Draw") 
+            elif player_hand == 3:
+                print("You Win!")   
+                
+        # Scissors (3)   
+        elif cpu_hand == 'Scissors':    
+            if player_hand == 1:
+                print("You Lose!")
+            elif player_hand == 2:
+                print("You Win!") 
+            elif player_hand == 3:
+                print("It Is A Draw")     
+    
+        print()
+    
+if __name__ == "__main__":
+    main()
 
